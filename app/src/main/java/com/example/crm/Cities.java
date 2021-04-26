13a80642567b88e54041b80df9f46f249adcb42a
flaxen-infosoft <akshaythakur0308@gmail.com>
2021-04-26 17:26:52 +0530

    Created EmployeeRegistration

--- app/src/main/java/com/example/crm/Cities.java
+++ app/src/main/java/com/example/crm/Cities.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class Cities {
-    String District;
+    String City;
     String State;
 }
--- app/src/main/java/com/example/crm/EmployeeRegistration.java
+++ app/src/main/java/com/example/crm/EmployeeRegistration.java
@@ -18,20 +18,46 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class CustomerRegisteration extends AppCompatActivity {
-
-    Spinner stateSpin, citySpin;
+public class EmployeeRegistration extends AppCompatActivity {
+    Spinner stateSpin, citySpin, categoryspin, departmentspin, genderspin, statusspin;
     List<String> stateList = new ArrayList<>();
     List<String> cityList = new ArrayList<>();
+    List<String> categoryList = new ArrayList<>();
+    List<String> departmentList = new ArrayList<>();
+    List<String> genderList = new ArrayList<>();
+    List<String> statusList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_registeration);
-
+        setContentView(R.layout.activity_employee_registration);
         stateSpin = findViewById(R.id.state);
         citySpin = findViewById(R.id.city);
-
+        categoryspin = findViewById(R.id.empcategory);
+        departmentspin = findViewById(R.id.empdept);
+        genderspin = findViewById(R.id.gender);
+        statusspin = findViewById(R.id.status);
+        categoryList.add("Internship");
+        categoryList.add(0, "Select Category");
+        categoryList.add("Job");
+        departmentList.add("Support");
+        departmentList.add(0, "Select Department");
+        departmentList.add("Documentation");
+        departmentList.add("HR");
+        departmentList.add("Development");
+        departmentList.add("Sales");
+        departmentList.add("Marketing");
+        genderList.add("Male");
+        genderList.add(0, "Select Gender");
+        genderList.add("Female");
+        genderList.add("Other");
+        statusList.add("Active");
+        statusList.add(0, "Select Status");
+        statusList.add("Inactive");
+        categoryspin.setAdapter(new ArrayAdapter<>(EmployeeRegistration.this, android.R.layout.simple_spinner_dropdown_item, categoryList));
+        departmentspin.setAdapter(new ArrayAdapter<>(EmployeeRegistration.this, android.R.layout.simple_spinner_dropdown_item, departmentList));
+        genderspin.setAdapter(new ArrayAdapter<>(EmployeeRegistration.this, android.R.layout.simple_spinner_dropdown_item, genderList));
+        statusspin.setAdapter(new ArrayAdapter<>(EmployeeRegistration.this, android.R.layout.simple_spinner_dropdown_item, statusList));
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://raw.githubusercontent.com/fayazara/Indian-Cities-API/master/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -52,8 +78,8 @@ public class CustomerRegisteration extends AppCompatActivity {
                 }
                 List<String> filteredStateList = removeDuplicates(stateList);
                 Collections.sort(filteredStateList);
-                filteredStateList.add(0,"Select State");
-                stateSpin.setAdapter(new ArrayAdapter<>(CustomerRegisteration.this, android.R.layout.simple_spinner_dropdown_item, filteredStateList));
+                filteredStateList.add(0, "Select State");
+                stateSpin.setAdapter(new ArrayAdapter<>(EmployeeRegistration.this, android.R.layout.simple_spinner_dropdown_item, filteredStateList));
                 stateSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -66,8 +92,8 @@ public class CustomerRegisteration extends AppCompatActivity {
                         }
                         List<String> filteredCityList = removeDuplicates(cityList);
                         Collections.sort(filteredCityList);
-                        filteredCityList.add(0,"Select City");
-                        citySpin.setAdapter(new ArrayAdapter<>(CustomerRegisteration.this, android.R.layout.simple_spinner_dropdown_item, filteredCityList));
+                        filteredCityList.add(0, "Select City");
+                        citySpin.setAdapter(new ArrayAdapter<>(EmployeeRegistration.this, android.R.layout.simple_spinner_dropdown_item, filteredCityList));
 
                     }
 
