99a725d37aaf72d7d55e2d0637a7fda291ddd880
flaxen-infosoft <akshaythakur0308@gmail.com>
2021-04-30 17:19:34 +0530

    Created InvoiceCreation

---
+++ app/src/main/java/com/example/crm/InvoiceCreation.java
@@ -0,0 +1,14 @@
+package com.example.crm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InvoiceCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice_creation);
+    }
+}
