ecf1714e84f00bf4dc70702da579aef24ebe9cf6
devuco <samyakagrawal13@gmail.com>
2021-05-03 10:22:15 +0530

    new layout

--- app/src/main/java/com/example/crm/UpdateCustomerRemark.java
+++ app/src/main/java/com/example/crm/UpdateCustomerRemark.java
@@ -1,19 +1,26 @@
 package com.example.crm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class UpdateCustomerRemark extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_customer_remark);
+        button = findViewById(R.id.update_customer);
 
-        Spinner spinner=findViewById(R.id.priority);
-        spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,new String[]{"Priority","Low","High","Critical","Emergency"}));
+        Spinner spinner = findViewById(R.id.priority);
+        spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, new String[]{"Priority", "Low", "High", "Critical", "Emergency"}));
+
+        button.setOnClickListener(v -> startActivity(new Intent(UpdateCustomerRemark.this,CustomerDetails.class)));
+
     }
 }
