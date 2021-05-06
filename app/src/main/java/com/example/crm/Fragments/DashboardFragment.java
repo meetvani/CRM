35cbff995276e131118d014f45aff7d1f47b2ef6
flaxen-infosoft <akshaythakur0308@gmail.com>
2021-05-06 15:23:42 +0530

    Changes in WebmailActivity

--- app/src/main/java/com/example/crm/Fragments/DashboardFragment.java
+++ app/src/main/java/com/example/crm/Fragments/DashboardFragment.java
@@ -1,7 +1,9 @@
 package com.example.crm.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,13 +11,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.crm.R;
+import com.example.crm.WebmailActivity;
 
 public class DashboardFragment extends Fragment {
-
+CardView webmailcard;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View v=inflater.inflate(R.layout.fragment_dashboard, container, false);
+        webmailcard=v.findViewById(R.id.webmail);
+        webmailcard.setOnClickListener(view -> {
+            startActivity(new Intent(getContext(), WebmailActivity.class));
+        });
+        return v;
     }
 }
--- app/src/main/java/com/example/crm/WebmailActivity.java
+++ app/src/main/java/com/example/crm/WebmailActivity.java
@@ -1,14 +1,61 @@
 package com.example.crm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.crm.Fragments.ArchieveFragment;
+import com.example.crm.Fragments.DraftFragment;
+import com.example.crm.Fragments.InboxFragment;
+import com.example.crm.Fragments.JunkFragment;
+import com.example.crm.Fragments.TrashFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class WebmailActivity extends AppCompatActivity {
-
+BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_webmail);
+        setContentView(R.layout.webmail);
+        bottomNavigationView=findViewById(R.id.bottomnav);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame1, new InboxFragment()).commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
     }
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+
+                case R.id.inbox:
+                    selectedFragment = new InboxFragment();
+                    break;
+
+                case R.id.draft:
+                    selectedFragment = new DraftFragment();
+                    break;
+
+                case R.id.trash:
+                   selectedFragment = new TrashFragment();
+                    break;
+                case R.id.junk:
+                    selectedFragment= new JunkFragment();
+                    break;
+                case R.id.archieve:
+                    selectedFragment=new ArchieveFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame1, selectedFragment).commit();
+
+            return true;
+        }
+    };
+
 }
